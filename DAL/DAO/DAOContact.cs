78d30232a7b748747e3552f1a75668ec979b432d
Unknown <ligno19@gmail.com>
2018-01-29 13:01:15 -0500

    Update Gitignore
    Etant donné que l'on a pas le meme connection string il faut le separer du gitignore
    Utiliser le fichier ConnectData

--- DAL/DAO/DAOContact.cs
+++ DAL/DAO/DAOContact.cs
@@ -12,12 +12,7 @@ namespace DAL.DAO
 
         public DAOContact()
         {
-            this.connection = new SqlConnection(
-                connectionString: @"Data Source=MESTA;
-                                    Initial Catalog=dbtpcontact;
-                                    Integrated Security=True;
-                                    Connect Timeout=5;"
-            );
+            this.connection = new SqlConnection( connectionString: ConnectData.connectionString );
         }
 
         public void Create(Contact contact)
