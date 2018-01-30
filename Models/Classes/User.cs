9a04160959970a4181a439d99104b6f4f9cd9620
Unknown <ligno19@gmail.com>
2018-01-29 19:06:07 -0500

    User begin

--- Models/Classes/User.cs
+++ Models/Classes/User.cs
@@ -7,11 +7,25 @@ using System.Threading.Tasks;
 namespace Models.Classes {
     class User {
 
-        public bool IsConnect { get; private set; }
-        public string Login { get; private set; }
-        public string Password { get; private set; }
+        public static bool IsConnect        = false;
+        public static long? current_user_id = null;
+
+        public long?    Id        { get; private set; }
+        public string   Login     { get; private set; }
+        public int      Password  { get; private set; }
+        public string   Email     { get; private set; }
 
         public long[] ContactId { get; private set; }
+
+        public User(string login, string password, string email) {
+            this.Login      = login;
+            this.Password   = Encrypt(password);
+            this.Email      = email;
+        }
+
+        private int Encrypt(string password) {
+            return password.GetHashCode();
+        }
 
 
     }
