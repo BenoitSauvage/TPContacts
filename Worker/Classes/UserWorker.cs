6f069844e509977b4f5b0bc69a5bc811245590b0
Unknown <ligno19@gmail.com>
2018-02-05 18:43:32 -0500

    Create user UI & Worker OK

--- Worker/Classes/UserWorker.cs
+++ Worker/Classes/UserWorker.cs
@@ -23,6 +23,32 @@ namespace Worker.Classes {
             return user.Login;
         }
 
+        public bool CheckExistLogin(string login) {
+            bool res = false;
+
+            List<User> users = userManager.FindAll();
+
+            for (int i=0; i<users.Count && !res; i++) {
+                if(users[i].Login == login) {
+                    res = true;
+                }
+
+            }
+
+
+            return res;
+        }
+
+        public void CreateUser(string login, string password, string firstname, string lastname,string email, string phone) {
+
+            User user = new User(login, password);
+            Contact contact = new Contact(firstname, lastname, email, phone);
+
+            userManager.Create(user, contact);
+
+        }
+
+
         public byte CheckConnect(string login, string password) {
             byte res = 0;
             bool stopLoop = false;
